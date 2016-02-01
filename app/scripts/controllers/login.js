8b1ca00a7f5873178e5d982b682c5b012c58267f
Kodah <tom_1440@hotmail.co.uk>
2016-02-01 20:37:04 +0000

    Fix path and login form

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,24 +8,24 @@
 * Controller of the assignmentApp
 */
 angular.module('assignmentApp')
-.controller('LoginCtrl', function ($scope, $http) {
+.controller('LoginCtrl', function ($scope, $http, $location) {
 
 	$scope.user = {};
 
-	// $scope.login = function(){
-	// 	$http({
-	// 		method: 'POST',
-	// 		url: 'http://localhost:3000/auth/login',
-	// 		data: JSON.stringify($scope.user),
-	// 		headers: {'Content-Type': 'application/json'
-	// 		}
-	// 	}).then(function successCallback(response) {
-	// 		console.log('Reg successful');
-	// 		$location.path('/login');
+	$scope.login = function(){
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/authentication',
+			data: JSON.stringify($scope.user),
+			headers: {'Content-Type': 'application/json'
+			}
+		}).then(function successCallback(response) {
+			console.log('Login successful -' + response.data);
+			// $location.path('/login');
 
-	// 	}, function errorCallback(response) {
-	// 		console.log(response);
-	// 	});
-	// };
+		}, function errorCallback(response) {
+			console.log(response);
+		});
+	};
 
 });
